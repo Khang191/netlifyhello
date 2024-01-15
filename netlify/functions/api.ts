9fb0e2b9228607668f7d3b308dc2f0a7cf5dd539
Khang Nguyen <khang.nguyen2@mekle.com>
2024-01-15 08:58:07 +0700

    Netlify hello 6

---
+++ netlify/functions/api.ts
@@ -0,0 +1,14 @@
+import cors from 'cors'
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const api = express();
+api.use(cors())
+
+const router = Router();
+
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
