78d63e4a2d6f5ee42e60f42d1daa7827967d4506
Khang Nguyen <khang.nguyen2@mekle.com>
2024-01-15 11:34:54 +0700

    Netlify hello 13

--- netlify/functions/src/routes/plant-check.ts
+++ netlify/functions/src/routes/plant-check.ts
@@ -5,12 +5,8 @@ const router = Router()
 router.get(
     '/plant-check',
     (req, res) => {
-        const rs = {
-            statusCode: 200,
-            message: "ok"
-        }
 
-        res.status(200).json(rs)
+        res.status(200)
     }
 )
 
