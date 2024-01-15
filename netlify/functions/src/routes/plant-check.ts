f74bc0d304148c259e1461a00a5cc6eec6f94abe
Khang Nguyen <khang.nguyen2@mekle.com>
2024-01-15 13:57:25 +0700

    Response susscessfully

--- netlify/functions/src/routes/plant-check.ts
+++ netlify/functions/src/routes/plant-check.ts
@@ -6,19 +6,38 @@ router.post(
     '/plant-check',
     (req, res) => {
 
-        res.status(400).json({
-            "statusCode": 400,
-            "message": "First error message.",
-            "errors": [
-                {
-                    "code": "SyntaxError",
-                    "message": "First error message."
-                },
-                {
-                    "code": "SemanticError",
-                    "message": "Second error message."
-                }
-            ]
+        res.status(201).json({
+            "type": "Cart",
+            "id": "27b39077-aa57-48a5-b504-914f68fa44dc",
+            "version": 1,
+            "createdAt": "2023-01-23T13:06:28.569Z",
+            "lastModifiedAt": "2023-01-23T13:06:28.569Z",
+            "lastModifiedBy": {
+                "isPlatformClient": false
+            },
+            "createdBy": {
+                "isPlatformClient": false
+            },
+            "lineItems": [],
+            "cartState": "Active",
+            "totalPrice": {
+                "type": "centPrecision",
+                "currencyCode": "EUR",
+                "centAmount": 0,
+                "fractionDigits": 2
+            },
+            "shippingMode": "Single",
+            "shipping": [],
+            "customLineItems": [],
+            "discountCodes": [],
+            "directDiscounts": [],
+            "inventoryMode": "None",
+            "taxMode": "Platform",
+            "taxRoundingMode": "HalfEven",
+            "taxCalculationMode": "LineItemLevel",
+            "refusedGifts": [],
+            "origin": "Customer",
+            "itemShippingAddresses": []
         })
     }
 )
