9ffee862bc9bdbea365c564c02daca36f3de63a3
Khang Nguyen <khang.nguyen2@mekle.com>
2024-01-15 14:22:15 +0700

    Response susscessfully 5

--- netlify/functions/src/routes/plant-check.ts
+++ netlify/functions/src/routes/plant-check.ts
@@ -6,40 +6,7 @@ router.post(
     '/plant-check',
     (req, res) => {
 
-        res.status(201).json({
-            "action": "addLineItem",
-            "type": "Cart",
-            "id": "27b39077-aa57-48a5-b504-914f68fa44dc",
-            "version": 1,
-            "createdAt": "2023-01-23T13:06:28.569Z",
-            "lastModifiedAt": "2023-01-23T13:06:28.569Z",
-            "lastModifiedBy": {
-                "isPlatformClient": false
-            },
-            "createdBy": {
-                "isPlatformClient": false
-            },
-            "lineItems": [],
-            "cartState": "Active",
-            "totalPrice": {
-                "type": "centPrecision",
-                "currencyCode": "EUR",
-                "centAmount": 0,
-                "fractionDigits": 2
-            },
-            "shippingMode": "Single",
-            "shipping": [],
-            "customLineItems": [],
-            "discountCodes": [],
-            "directDiscounts": [],
-            "inventoryMode": "None",
-            "taxMode": "Platform",
-            "taxRoundingMode": "HalfEven",
-            "taxCalculationMode": "LineItemLevel",
-            "refusedGifts": [],
-            "origin": "Customer",
-            "itemShippingAddresses": []
-        })
+        res.status(201).json()
     }
 )
 
