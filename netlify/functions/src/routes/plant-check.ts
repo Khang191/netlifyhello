5e622d0f71647a16dbfe1a9f4e9c0dd3808f0669
Khang Nguyen <khang.nguyen2@mekle.com>
2024-01-15 12:36:48 +0700

    Netlify hello 20

--- netlify/functions/src/routes/plant-check.ts
+++ netlify/functions/src/routes/plant-check.ts
@@ -6,15 +6,20 @@ router.post(
     '/plant-check',
     (req, res) => {
 
-        res.status(400).json( {
-                "code": "ExtensionPredicateEvaluationFailed",
-                "message": "The compared field 'cart' is not present",
-                "errorByExtension": {
-                    "id": "f0b48b75-f375-4d26-a747-691e877bb0ff",
-                    "key": "my-extension"
+        res.status(400).json({
+            "statusCode": 400,
+            "message": "First error message.",
+            "errors": [
+                {
+                    "code": "SyntaxError",
+                    "message": "First error message."
+                },
+                {
+                    "code": "SemanticError",
+                    "message": "Second error message."
                 }
-            }
-        )
+            ]
+        })
     }
 )
 
