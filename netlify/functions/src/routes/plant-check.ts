4c9f578f004ceac4c95fbdd871256264b2cb3cc3
Khang Nguyen <khang.nguyen2@mekle.com>
2024-01-15 12:30:12 +0700

    Netlify hello 18

--- netlify/functions/src/routes/plant-check.ts
+++ netlify/functions/src/routes/plant-check.ts
@@ -7,9 +7,12 @@ router.post(
     (req, res) => {
 
         res.status(400).json( {
-                statusCode: 400,
-                message: 'Cannot create a cart',
-                errors: []
+                "code": "ExtensionPredicateEvaluationFailed",
+                "message": "The compared field 'cart' is not present",
+                "errorByExtension": {
+                    "id": "{{extension-id}}",
+                    "key": "{{extension-key}}"
+                }
             }
         )
     }
