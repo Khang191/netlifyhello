77591ec0b9a37cb68b84c376a17bb4e2468c8ac5
Khang Nguyen <khang.nguyen2@isobar.com>
2024-01-18 16:56:47 +0700

    Api movies 1

--- netlify/functions/src/routes/index.ts
+++ netlify/functions/src/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express"
 import hello from './plant-check'
+import movies from './movies'
 
 const router = Router();
 router.use(hello);
+router.use(movies);
+
 export default router;
