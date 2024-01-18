400afc03bd1f0b51d798be6ed1d3d4f64dcedace
Khang Nguyen <khang.nguyen2@isobar.com>
2024-01-18 17:33:49 +0700

    Api movies 3

--- netlify/functions/src/controller/MovieController.ts
+++ netlify/functions/src/controller/MovieController.ts
@@ -5,11 +5,20 @@ class MovieController {
     constructor() {}
 
     async getMoviesById(req: Request, res: Response) {
-        const {id} = req.body
+        const { id } = req.body
 
-        res.status(200).json({
-            openingCrawl: "Data from api netlify by Khang!!!!!!!!!!!!!!!!!!"
-        })
+        const rs = {
+            data: {
+                film: {
+                    episodeID: 4,
+                    openingCrawl: "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....",
+                    releaseDate: "1977-05-25",
+                    title: "A New Hope"
+                }
+            }
+        }
+
+        res.status(200).json(rs)
 
         // if (customer?.statusCode === 200 && fields?.plantCheck == 1) {
         //     res.status(400).json({
