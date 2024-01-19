8ac56f811d4d8b0e91eafda7dea3f6c5b95fd1b2
Khang Nguyen <khang.nguyen2@mekle.com>
2024-01-19 09:49:10 +0700

    movie api

--- netlify/functions/src/controller/MovieController.ts
+++ netlify/functions/src/controller/MovieController.ts
@@ -9,12 +9,10 @@ class MovieController {
 
         const rs = {
             data: {
-                film: {
-                    episodeID: 4,
-                    openingCrawl: "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....",
-                    releaseDate: "1977-05-25",
-                    title: "A New Hope"
-                }
+                episodeID: 4,
+                openingCrawl: "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....",
+                releaseDate: "1977-05-25",
+                title: "A New Hope"
             }
         }
 
